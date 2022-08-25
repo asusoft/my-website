275989995e25a24445eedb1780002f2622a37573
Shehu Abubakar Umar <asusoft19@gmail.com>
2022-08-25 18:55:57 +0300

    Create Resume model

--- account/models.py
+++ account/models.py
@@ -1,3 +1,13 @@
+from turtle import title
 from django.db import models
+from django.core.validators import URLValidator
 
 # Create your models here.
+
+class Resume(models.Model):
+    name = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    about = models.TextField()
+    picture = models.ImageField(upload_to='pics')
+    cv = models.TextField(validators=[URLValidator()])
